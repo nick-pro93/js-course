21741264c3a6ddfcc25c8cb09ce3191f0aa690e8
nick-pro93 <gr.partners.ltd@gmail.com>
2021-09-06 00:56:03 +0600

    lesson if else

--- script.js
+++ script.js
@@ -42,3 +42,39 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+if (4 == 4) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+const num = 101;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+}
+
+const num = 50;
+(num === 50) ? console.log('Ok!') : console.log('Error');
+
+const num1=177;
+
+switch (num1) {
+    case 49:
+        console.log ('Неверно');
+        break;
+    case 100:
+        console.log ('Неверно');
+        break;
+    case 50:
+        console.log ("В точку!")
+        break;
+    default:
+        console.log ('Не в этот раз');
+        break;
+}
