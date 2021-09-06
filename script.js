19ffffa27da99e344de5ced2ba6d8a6b60133dcc
nick-pro93 <gr.partners.ltd@gmail.com>
2021-09-06 21:29:49 +0600

    lesson 15 1 task

--- script.js
+++ script.js
@@ -41,6 +41,11 @@ const a = prompt('Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+}
 console.log(personalMovieDB);
 
 if (4 == 4) {
